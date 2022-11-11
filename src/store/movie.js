f1f5c50512dadbe56fbbccdfb52d963d6cf3f737
Minhyuk Park <parkminem@gmail.com>
2022-11-11 18:52:25 +0900

    fix

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,19 +1,21 @@
 import axios from 'axios'
 import { defineStore } from 'pinia'
-// import { ref } from 'vue'
+import { ref } from 'vue'
 
 export const useSearch = defineStore('search', () => {
   const OMDB_API_KEY = '366ae844'
-  // const moives = ref([])
+  const movies = ref([])
 
   async function searchMovies(payload) {
-    const { title, type, year } = payload
+    const { title, type, number, year } = payload
     const res = await axios.get(
       `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
     )
     console.log(res)
+    return res
   }
   return {
+    movies,
     searchMovies
   }
 })
